44a2c21893a4191ea910a4b57793856d4f25e68b
Robert Williams <robert@a3marketing.com>
2020-03-21 10:47:35 -0500

    added new api key for google

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,12 @@
-var userStart = $("#inputStart").val().trim()
-var userEnd = $("#inputDestination").val().trim()
+var userStart = $("#inputStart").val()
+var userEnd = $("#inputDestination").val()
 var submitBtn = $("#submitBtn")
 // taste dive api information
-var apiKey1 = "AIzaSyDgOE3QM2wQZZgOrdOyhBuX_GF-bgZGhU4"
-var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?origin=" + userStart + "&destination=" + userEnd + "&key=" + apiKey1
+var apiKey1 = "AIzaSyBan-c0mRECIQzo4yd6oGeBeosVcJNFaPw"
+// var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?origin=" + userStart + "&destination=" + userEnd + "&key=" + apiKey1
+
+var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?origin=3201+Dickerson+Pike&destination=3441+Dickerson+Pike&key=" + apiKey1
+
 // omdb api information
 
 
